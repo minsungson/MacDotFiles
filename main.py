45a534db3dbee812d2d0d6ccf03f6a45af526df4
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-05-07 22:25:27 +0100

    url corrections

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ webbrowser.open("https://dl.google.com/chrome/mac/universal/stable/GGRO/googlech
 webbrowser.open("https://az764295.vo.msecnd.net/stable/57fd6d0195bb9b9d1b49f6da5db789060795de47/VSCode-darwin-universal.zip") # latest VS Code
 webbrowser.open("https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_16.60.22041000_Installer.pkg") # Office 16.60
 webbrowser.open("https://dl.strem.io/shell-osx/v4.4.142/Stremio+4.4.142.dmg") # stremio 4.4
-webbrowser.open("https://download.skype.com/s4l/download/mac/Skype-8.83.0.") # skype 8.83
-webbrowser.open("https://objects.githubusercontent.com/github-production-release-asset-2e65be/768345/bf36b9d7-9860-4675-a533-60f2599f6e4d?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20220507%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220507T210919Z&X-Amz-Expires=300&X-Amz-Signature=357398b24b989a0d348597c72cae68169fcb048f9212567ba26a63ea366d6691&X-Amz-SignedHeaders=host&actor_id=100154286&key_id=0&repo_id=768345&response-content-disposition=attachment%3B%20filename%3DSpectacle%2B1.2.zip&response-content-type=application%2Foctet-stream") # spectacle
+webbrowser.open("https://download.skype.com/s4l/download/mac/Skype-8.83.0.408.dmg") # skype 8.83
+webbrowser.open("https://github.com/eczarny/spectacle/releases/download/1.2/Spectacle+1.2.zip") # spectacle
 webbrowser.open("https://apps.apple.com/app/bitwarden/id1352778147") # bitwarden
 webbrowser.open("https://protonvpn.com/download/ProtonVPN.dmg") # latest protonvpn
 webbrowser.open("https://cdn.last.fm/client/Mac/Last.fm-2.1.39.zip") # last.fm 2.1.39
