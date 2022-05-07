5892eb84f34883e0b8bd12e45e09a06d9f09cc03
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-05-07 22:07:41 +0100

    delete all

--- main.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-
-webbrowser.open("https://www.mozilla.org/en-US/firefox/download/thanks/") # latest firefox
-webbrowser.open("https://calibre-ebook.com/dist/osx") # latest calibre
-webbrowser.open("https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg") # latest chrome
-webbrowser.open("https://az764295.vo.msecnd.net/stable/57fd6d0195bb9b9d1b49f6da5db789060795de47/VSCode-darwin-universal.zip") # latest VS Code
-webbrowser.open("https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_16.60.22041000_Installer.pkg") # Office 16.60
-webbrowser.open("https://dl.strem.io/shell-osx/v4.4.142/Stremio+4.4.142.dmg") # stremio 4.4
-webbrowser.open("https://download.skype.com/s4l/download/mac/Skype-8.83.0.408.dmg") # skype 8.83
