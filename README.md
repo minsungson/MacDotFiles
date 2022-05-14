3662bbec22cb02b94c136bb5b115a70f2bc8f627
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-05-14 12:46:33 +0000

    office 2019 latest

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # For Setting up a new Mac
 [![Build Status](https://app.travis-ci.com/minsungson/Mac-Setup-Script.svg?token=KNqCttmBXxyZdi57sDwp&branch=master)](https://app.travis-ci.com/minsungson/Mac-Setup-Script)
 
-Run
+##Steps
+Install Homebrew
+```
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 ```
 curl -O https://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@raw.githubusercontent.com/minsungson/Mac-Setup-Script/master/main.py && /usr/local/bin/python3 /Users/minsungson/main.py
 ```
@@ -18,4 +21,3 @@ Installs:
 - last.fm 2.1.39
 - ProtonVPN
 - Bitwarden
-- Malwarebyte
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ webbrowser.open("https://www.mozilla.org/en-US/firefox/download/thanks/") # late
 webbrowser.open("https://calibre-ebook.com/dist/osx") # latest calibre
 webbrowser.open("https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg") # latest chrome
 webbrowser.open("https://az764295.vo.msecnd.net/stable/57fd6d0195bb9b9d1b49f6da5db789060795de47/VSCode-darwin-universal.zip") # latest VS Code
-webbrowser.open("https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_16.60.22041000_Installer.pkg") # Office 16.60
+webbrowser.open("https://go.microsoft.com/fwlink/p/?linkid=525133") # Office 2019 latest
 webbrowser.open("https://dl.strem.io/shell-osx/v4.4.142/Stremio+4.4.142.dmg") # stremio 4.4
 webbrowser.open("https://download.skype.com/s4l/download/mac/Skype-8.83.0.408.dmg") # skype 8.83
 webbrowser.open("https://github.com/eczarny/spectacle/releases/download/1.2/Spectacle+1.2.zip") # spectacle
