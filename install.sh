f8661d7b7d594545373795f633038705ea6949dc
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-11-17 10:53:12 +0000

    fix syntax

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo "Installing Brew..."
 brew analytics off
 brew upgrade
 brew install mas
-brew install amethyst
+brew install -- cask amethyst
 
 # # Mac App Store Apps
 echo "Installing Mac App Store Apps..."
